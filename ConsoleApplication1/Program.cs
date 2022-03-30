6d08321fe20fac60a72c67a115ea8aea45121e08
jonowak <jnowak@optionmetrics.com>
2022-03-30 16:20:51 -0400

    old school stuff

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckDigit
+{
+    class CheckDigit
+    {
+        static void Main(string args)
+        {
+            string cDigit;
+            cDigit = GenerateCheckDigit(args);
+        }
+
+        public static string GenerateCheckDigit(string cusip)
+        {
+            int sum = 0;
+            char[] digits = cusip.ToUpper().ToCharArray();
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ*@#";
+
+            for (int i = 0; i < digits.Length; i++)
+            {
+                int val;
+                if (!int.TryParse(digits[i].ToString(), out val))
+                    val = alphabet.IndexOf(digits[i]) + 10;
+
+                if ((i % 2) != 0)
+                    val *= 2;
+
+                val = (val % 10) + (val / 10);
+
+                sum += val;
+            }
+
+            int check = (10 - (sum % 10)) % 10;
+
+            return check.ToString();
+        }
+    }
+}
